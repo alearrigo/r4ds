5d58bc84302da4d0ea2c3c79262dd40ba3d25b10
Alessandro Arrigo <alessandroarrigo@me.com>
2020-03-19 19:06:13 +0100

    Second Commit

--- Chapter 1 - Explore ale.R
+++ Chapter 1 - Explore ale.R
@@ -7,12 +7,84 @@ data <- mpg
 
 str(data)
 
-#hwy -----> fuel efficiency
-#displ -----> engine size
+# displ -----> engine size, in litres
+# drv -----> drive train (f= front-wheel drive, r = rear wheel drive, 4= 4wd)
+# hwy -----> highway miles per gallon
+# fl -----> fuel type
+# class -----> "type" of car
 
 
 apply(data, 2, unique)
 
+data %>% nrow()
 
 ggplot(data=mpg)+
   geom_point(mapping = aes(x=displ,y=hwy))
+
+ggplot(data=mpg)+
+  geom_point(mapping = aes(x=class,y=drv))
+
+
+#plottiamo per vedere a che categoria appartengono i punti
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x = displ, y = fl, color= drv, size = hwy))
+
+
+ggplot(data=mpg)+
+  geom_point(mapping = aes(x=displ, y=hwy, color= displ<5))
+
+
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x = drv, y = cyl))
+
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x = displ, y = hwy)) +
+  facet_grid(drv ~ cyl)
+
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x = displ, y = hwy)) +
+  facet_grid(drv ~ .)
+
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x = displ, y = hwy)) +
+  facet_wrap(~cyl, nrow = 1)
+
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x = displ, y = hwy)) +
+  facet_grid(.~ cyl)
+
+ggplot(data = mpg) +
+  geom_smooth(mapping = aes(x = displ, y = hwy))
+
+ggplot(data = mpg) +
+  geom_smooth(mapping = aes(x = displ, y = hwy, group = drv))
+
+ggplot(data = mpg) +
+  geom_smooth(mapping = aes(x = displ, y = hwy, color = drv))+
+  geom_point(aes(x = displ, y = hwy, color = drv))
+
+ggplot(data = mpg, mapping = aes(x = displ, y = hwy)) +
+  geom_point() +
+  geom_smooth()
+
+ggplot(data = mpg, mapping = aes(x = displ, y = hwy)) +
+  geom_point(mapping = aes(color = class)) +
+  geom_smooth()
+
+
+ggplot(data = diamonds) +
+  stat_summary(
+    mapping = aes(x = cut, y = depth),
+    fun.ymin = min,
+    fun.ymax = max,
+    fun.y = median
+  )
+
+
+library(tidyverse)
+
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x = displ, y = hwy))
+
+filter(mpg, cyl == 8)
+filter(diamonds, carat > 3)
